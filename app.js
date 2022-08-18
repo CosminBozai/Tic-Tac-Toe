25a0df46d970b8f4fdcea31fbc6548d21dca3c59
CosminBozai <cosminbozai@outlook.com>
2022-08-18 13:37:01 +0100

    Check for win
    Added function that checks for winning pattern in the classes of cells
    Added funtions to show and hide the overscreen

--- app.js
+++ app.js
@@ -11,7 +11,19 @@ const Gameboard = (() => {
 })();
 
 const overScreenHandler = (() => {
+  const overScreen = document.getElementById("overscreen");
+  const hide = () => {
+    overScreen.classList.remove("show");
+  };
+  const show = () => {
+    overScreen.classList.add("show");
+  };
+  return {
+    hide,
+    show,
+  };
   /*
+
    */
 })();
 
@@ -27,10 +39,32 @@ const Game = (() => {
       } else if (circleTurn == true) {
         cell.classList.add("circle");
       }
+      checkWin();
       return (circleTurn = !circleTurn);
     });
   });
+
+  function checkWin() {
+    winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    return winningCombinations.some((combination) => {
+      return combination.every((index) => {
+        return Gameboard.getCells()[index].classList.contains("x");
+      });
+    });
+  }
+
   /*
   add event listener to all the cells
+  check for win
+  if checkwin display overscreen
    */
 })();
