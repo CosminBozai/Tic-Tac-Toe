3a915edf7fc31209e655ea9c5036a5093855a4c8
CosminBozai <cosminbozai@outlook.com>
2022-08-18 13:21:50 +0100

    Add mark and swap turns

--- app.js
+++ app.js
@@ -16,11 +16,18 @@ const overScreenHandler = (() => {
 })();
 
 const Game = (() => {
+  let circleTurn = false;
   // const playerOne = document.getElementById("palyer1").value;
   // const playerTwo = document.getElementById("palyer2").value;
   Gameboard.getCells().forEach((cell) => {
     cell.addEventListener("click", (e) => {
-      console.log(e.target);
+      cell = e.target;
+      if (circleTurn == false) {
+        cell.classList.add("x");
+      } else if (circleTurn == true) {
+        cell.classList.add("circle");
+      }
+      return (circleTurn = !circleTurn);
     });
   });
   /*
